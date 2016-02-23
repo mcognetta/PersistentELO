d685fdc83e48a13d6547b75a6a0023daca600679
Marco Cognetta <cognetta.marco@gmail.com>
2016-02-23 14:04:44 -0500

    Adding setup.py and relevant files as well as updating .gitignore to ignore the newly created source dist files.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='PersistentELO',
+	version='1.0',
+	author='Marco Cognetta',
+	author_email='cognetta.marco@gmail.com',
+	url='https://github.com/mcognetta/PersistentELO',
+	packages=['PersistentELO'],
+	description='Library for persistent ELO rankings')
